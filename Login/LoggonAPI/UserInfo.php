d81e9840cba1408a51fbe715f6aeacc46898148f
tehgunte <83183177+tehgunte@users.noreply.github.com>
2022-04-12 11:33:15 -0400

    Update UserInfo.php

--- Login/LoggonAPI/UserInfo.php
+++ Login/LoggonAPI/UserInfo.php
@@ -1,24 +1,33 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/game/ProdRBX/Configuration.php'); ?>
 <?php
-if (isset($_COOKIE['ROBLOSECURITY'])) {
-	$RBXTICKET = $_COOKIE['ROBLOSECURITY'];
-} else {
-	$RBXTICKET = null;
+switch(true){
+	case (isset($_COOKIE['ROBLOSECURITY'])):
+		$RBXTICKET = $_COOKIE['ROBLOSECURITY'];
+		break;
+	default:
+		$RBXTICKET = null;
+		break;
 }
 
-if ($RBXTICKET !== null) {
-	$GetInfo = $MainDB->prepare("SELECT id, name, ticket, robux, termtype, admin FROM users WHERE token = ?");
-	$GetInfo->bind_param("s", $RBXTICKET);
-	$GetInfo->execute();
-	$GetInfo->bind_result($id, $name, $ticket, $robux, $terminated, $admin);
-	$GetInfo->fetch();
-	$GetInfo->close();
-}else{
-	$id = null;
-	$name = null;
-	$ticket = null;
-	$robux = null;
-	$terminated = null;
-	$admin = null;
+switch(true){
+	case ($RBXTICKET !== null):
+		$GetInfo = $MainDB->prepare("SELECT id, name, ticket, robux, termtype, admin FROM users WHERE token = :token");
+		$GetInfo->execute([':token' => $RBXTICKET]);
+		$Info = $GetInfo->fetch(PDO::FETCH_ASSOC);
+		$id = $Info['id'];
+		$name = $Info['name'];
+		$ticket = $Info['ticket'];
+		$robux = $Info['robux'];
+		$termtype = $Info['termtype'];
+		$admin = $Info['admin'];
+		break;
+	default:
+		$id = null;
+		$name = null;
+		$ticket = null;
+		$robux = null;
+		$termtype = null;
+		$admin = null;
+		break;
 }
 ?>
