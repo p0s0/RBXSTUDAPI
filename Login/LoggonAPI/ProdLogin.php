69db911cb1b9ef1b2713a8c3918d1af5c2b6a523
tehgunte <83183177+tehgunte@users.noreply.github.com>
2022-04-12 11:25:48 -0400

    Update ProdLogin.php

--- Login/LoggonAPI/ProdLogin.php
+++ Login/LoggonAPI/ProdLogin.php
@@ -4,32 +4,42 @@ session_start();
 $errors = array();
 $date = date("Y-m-d");
 
-if (isset($_POST['loginrbx'])) {
-	$username = mysqli_real_escape_string($MainDB, strip_tags($_POST['username']));
-	$password = mysqli_real_escape_string($MainDB, strip_tags($_POST['password']));
-	
-	if (empty($username)) {array_push($errors, "Username box is empty.");}
-	if (empty($password)) {array_push($errors, "Password box is empty.");}
-	if (preg_match('/^[a-z0-9_]+$/i', $username) == 0) {array_push($errors, "User does not exist.");}
-	if (preg_match('/^[a-z0-9_]+$/i', $password) == 0) {array_push($errors, "User does not exist.");}
-	
-    $loggon = $MainDB->prepare("SELECT password, token FROM users WHERE name=?");
-    $loggon->bind_param("s", $username);
-    $loggon->execute();
-    $loggon->bind_result($checkpsw, $token);
-    $loggon->fetch();
-    
-	if (count($errors) == 0) {  
-		if (!empty($checkpsw)){
-			if(password_verify($password,$checkpsw)){
-				setcookie("ROBLOSECURITY", $token, time()+9900, "/", $_SERVER['SERVER_NAME']);
-				header("Refresh:0");
-			}else{
-			array_push($errors, "The password is not right.");
-			}
-		}else{
-		array_push($errors, "User does not exist.");
+switch(true){
+	case (isset($_POST['loginrbx'])):
+		$username = strip_tags($_POST['username']);
+		$password = strip_tags($_POST['password']);
+		
+		switch(true){case (empty($username)):array_push($errors, "Username box is empty.");break;}
+		switch(true){case (empty($username)):array_push($errors, "Password box is empty.");break;}
+		switch(true){case (preg_match('/^[a-z0-9_]+$/i', $username) == 0):array_push($errors, "User does not exist.");break;}
+		switch(true){case (preg_match('/^[a-z0-9_]+$/i', $password) == 0):array_push($errors, "User does not exist.");break;}
+
+		$loggon = $MainDB->prepare("SELECT password, token FROM users WHERE name = :username");
+		$loggon->execute([':username' => $username]);
+		$results = $loggon->fetch(PDO::FETCH_ASSOC);
+		$checkpsw = $results['password'];
+		$token = $results['token'];
+		
+		switch(true){
+			case (count($errors) == 0):
+				switch(true){
+					case (!empty($checkpsw)):
+						switch(true){
+							case (password_verify($password,$checkpsw)):
+								setcookie("ROBLOSECURITY", $token, time()+9900, "/", $_SERVER['SERVER_NAME']);
+								header("Refresh:0");
+								break;
+							default:
+								array_push($errors, "Your password is incorrect.");
+								break;
+						}
+						break;
+					default:
+						array_push($errors, "This account does not exist.");
+						break;
+				}
+				break;
 		}
-	}
+		break;
 }
 ?>
